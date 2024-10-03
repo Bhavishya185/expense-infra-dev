acb71628578d28f30f5cb61e7d627a07b3118f5b
Bhavishya <bhavishyachowdary185@gmail.com>
2024-10-03 14:10:38 -0400

    user profile

--- 50-vpn/data.tf
+++ 50-vpn/data.tf
@@ -1,6 +1,5 @@
- data "aws_ssm_parameter" "bastion_sg_id" {
-  #/expense/dev/bastion_sg_id
-  name = "/${var.project_name}/${var.environment}/bastion_sg_id"
+ data "aws_ssm_parameter" "vpn_sg_id" {
+  name = "/${var.project_name}/${var.environment}/vpn_sg_id"
 }
 
 data "aws_ssm_parameter" "public_subnet_ids" {
